88221e87b20158b02a4ca022654cd55e258965ee
LeCarlo <36467682+LeCarlo@users.noreply.github.com>
2019-04-11 14:51:25 +1000

    Movement Scrip and debug camera script.
    - Debug Camera script can also be used as a top down view of whoever is in front
    - Movement script has a bool variable for player 2

---
+++ RatRun/Assets/Scripts/DebugCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugCamera : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject inFront;
+    public GameObject debugCamera;
+    public float midpoint;
+	// Use this for initialization
+	void Start () {
+        player1 = GameObject.FindGameObjectWithTag("Player1");
+        player2 = GameObject.FindGameObjectWithTag("Player2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player1.transform.position.x > player2.transform.position.x)
+        {
+            inFront = player1;
+            midpoint = -3;
+        }
+        else //if (player2.transform.position.x < player1.transform.position.x)
+        {
+            inFront = player2;
+            midpoint = +3;
+        }
+        
+        transform.position = inFront.transform.position + new Vector3(0, 10, midpoint);
+        
+	}
+}
---
+++ RatRun/Assets/Scripts/Movement.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    // Use this for initialization
+    public float force;
+    public float horizontal;
+    public Vector3 moveForward;
+    public float speed;
+
+    public bool isPlayer2;
+    public Rigidbody m_rigidBody;
+    public float cooldownTimer;
+    public float slowdownSpeed;
+    void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!isPlayer2)
+        {
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) && cooldownTimer >= 0)
+            {
+
+                cooldownTimer = 10f;
+
+                if (horizontal >= 0 && horizontal <= 5)
+                {
+                    horizontal += 0.5f;
+                }
+                if (horizontal >= 5 && horizontal <= 10)
+                {
+                    horizontal += 0.25f;
+                }
+                if (horizontal >= 10f)
+                {
+                    horizontal = 10f;
+                }
+                moveForward = new Vector3(horizontal, 0, 0);
+                m_rigidBody.AddForce(moveForward * speed, ForceMode.Impulse);
+
+
+                //remmber the shit you did 2 months ago with force and velocity
+
+            }
+            else if (!Input.GetKeyDown(KeyCode.A) && !Input.GetKeyDown(KeyCode.D))
+            {
+                cooldownTimer -= 0.5f;
+                if (cooldownTimer <= 0)
+                {
+                    cooldownTimer = 0;
+                }
+                if (cooldownTimer == 0)
+                {
+                    if (horizontal <= 10 && horizontal >= 5)
+                    {
+                        horizontal -= 0.5f;
+                    }
+                    if (horizontal >= 0 && horizontal <= 5)
+                    {
+                        horizontal -= 20.0f;
+                    }
+                    if (horizontal <= 0f)
+                    {
+                        horizontal = 0;
+                        moveForward = new Vector3(horizontal, 0, 0);
+                        m_rigidBody.AddForce(moveForward * slowdownSpeed, ForceMode.Impulse);
+                    }
+                    
+                    m_rigidBody.AddForce(moveForward * speed, ForceMode.Impulse);
+
+
+                }
+            }
+        }
+
+        if (isPlayer2)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) && cooldownTimer >= 0)
+            {
+
+                cooldownTimer = 10f;
+
+                if (horizontal >= 0 && horizontal <= 5)
+                {
+                    horizontal += 0.5f;
+                }
+                if (horizontal >= 5 && horizontal <= 10)
+                {
+                    horizontal += 0.25f;
+                }
+                if (horizontal >= 10f)
+                {
+                    horizontal = 10f;
+                }
+                moveForward = new Vector3(horizontal, 0, 0);
+                m_rigidBody.AddForce(moveForward * speed, ForceMode.Impulse);
+
+
+                //remmber the shit you did 2 months ago with force and velocity
+
+            }
+            else if (!Input.GetKeyDown(KeyCode.LeftArrow) && !Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                cooldownTimer -= 0.5f;
+                if (cooldownTimer <= 0)
+                {
+                    cooldownTimer = 0;
+                }
+                if (cooldownTimer == 0)
+                {
+                    if (horizontal <= 10 && horizontal >= 5)
+                    {
+                        horizontal -= 0.5f;
+                    }
+                    if (horizontal >= 0 && horizontal <= 5)
+                    {
+                        horizontal -= 20.0f;
+                    }
+                    if (horizontal <= 0f)
+                    {
+                        horizontal = 0;
+                        moveForward = new Vector3(horizontal, 0, 0);
+                        m_rigidBody.AddForce(moveForward * slowdownSpeed, ForceMode.Impulse);
+                    }
+
+                    m_rigidBody.AddForce(moveForward * speed, ForceMode.Impulse);
+
+                }
+            }
+        }
+      
+	}
+}
