1eff7101307603ef5039d3254edc47823012decf
Kazwyrm <tlangford203@gmail.com>
2019-04-11 11:07:06 +1000

    Added Menu Scripts

---
+++ RatRun/Assets/Scripts/UI/Menus.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
