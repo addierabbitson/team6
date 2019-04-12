63e03b5920442f2a6647a313e5d46455224cd3a9
addierabbitson <39174595+addierabbitson@users.noreply.github.com>
2019-04-12 12:16:45 +1000

    Setup both rat animations.

---
+++ RatRun/Assets/Scripts/Player/Player1Animator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Animator : MonoBehaviour {
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.A) == true) {
+            GetComponent<Animator>().Play("Rat_Left_Anim");
+        }
+        else if (Input.GetKey(KeyCode.D) == true)
+        {
+            GetComponent<Animator>().Play("Rat_Right_Anim");
+        }
+        else if (Input.GetKey(KeyCode.D) == false && Input.GetKeyDown(KeyCode.A) == false)
+        {
+            GetComponent<Animator>().Play("Rat_Idle_Anim");
+        }
+    }
+}
---
+++ RatRun/Assets/Scripts/Player/Player2Animator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Animator : MonoBehaviour {
+	
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow) == true) {
+            GetComponent<Animator>().Play("Rat_Left_Anim");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            GetComponent<Animator>().Play("Rat_Right_Anim");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) == false && Input.GetKeyDown(KeyCode.RightArrow) == false)
+        {
+            GetComponent<Animator>().Play("Rat_Idle_Anim");
+        }
+    }
+}
